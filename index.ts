cd83277bbc0a8b3ece3081dbbe17ec58ccd74ea8
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-12-23 11:40:50 +0900

    fix

--- index.ts
+++ index.ts
@@ -4,18 +4,19 @@ import { Command } from 'commander'
 import { MetadataRetriever, Metadata } from "./metadata-retriever"
 import { statSync } from 'fs'
 
-const p = (() => {
+const root = (() => {
   try {
     statSync(__dirname + '/../package.json')
-    return require(__dirname + '/../package.json')
+    return __dirname + '/../'
   } catch (e) {
-    return require(__dirname + '/package.json')
+    return __dirname + '/'
   }
 })()
+const version = require(`${root}/package.json`).version
 
 const program = new Command()
 program
-  .version(p.version)
+  .version(version)
   .option('-d, --debug', 'output extra debugging')
   .option('-t, --template <template>', 'ejs template', './templates/sobject.js.ejs')
   .option('-T, --type <type>', 'SObject Type (e.g. Account)')
@@ -24,9 +25,9 @@ program.parse(process.argv)
 const user = process.env.SALESFORCE_USERNAME
 const pass = process.env.SALESFORCE_PASSWORD;
 const templateAlias: any = {
-  ts: './templates/sobject.ts.ejs',
-  js: './templates/sobject.js.ejs',
-  apex: './templates/sobject.cls.ejs',
+  ts: `${root}/templates/sobject.ts.ejs`,
+  js: `${root}/templates/sobject.js.ejs`,
+  apex: `${root}/templates/sobject.cls.ejs`,
 };
 
 (async () => {
